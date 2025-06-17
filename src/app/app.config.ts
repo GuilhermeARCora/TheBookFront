da277a798f0667e0d040a96fa3c6ddf74cb76c0c
Guilherme Corá <guilhermearcora@gmail.com>
2025-06-17 20:03:51 -0300

    Starting project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // provideHttpClient(withInterceptors([AuthInterceptor])),
+
+    // // AuthService so Angular can inject it
+    // AuthService,
+    // // Preload function to run at app startup
+    // {
+    //   provide: APP_INITIALIZER,
+    //   useFactory: preloadUser,
+    //   deps: [AuthService],
+    //   multi: true
+    // }
+  ]
 };
